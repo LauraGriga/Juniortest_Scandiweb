ffed3b1e3cec7a98b6155de56878589739152484
LauraGriga <larajuma@gmail.com>
2022-08-08 13:36:15 +0300

    Upgraded to Bootstrap

--- add-product.php
+++ add-product.php
@@ -3,19 +3,32 @@
         <title>Product Add</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Google fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+
+        <!--Stylesheets-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    
     </head>
 
     <!--Need to format with css that text and buttons is in one line-->
-    <nav>
-        <h3>Product Add</h3>
-        <div class = "buttonposition">
-        <button type="submit" onclick="save()" >Save</button> 
-        <button onclick="window.location.href='productlist.php'">Cancel</button>
-        </div>
-    </nav>
+    <div id="title">
+                <nav class="navbar">
+                    <div class="container-fluid">
+                    <a class="navbar-brand">Product list</a>
+                    <span class="navbar-item">
+                    <button class="btn btn-light btn-style" type="submit" onclick="save()" >Save</button>
+                    <button class="btn btn-light btn-style" onclick="window.location.href='productlist.php'">Cancel</button>
+                    </div>
+                </nav>
+            </div>
     <hr>
     <body>
+    
+    <div id="body">
 
     <?php 
     require_once 'connectdb.php';
@@ -68,38 +81,45 @@
         <!--Visual side(structure) of Add product form-->
 
            <form id="product_form" action="add-product.php" method="post" name="productform" >
-            <label for="sku" id="sku">SKU</label>
-            <input type="text" id="sku" name="sku" required="required" maxlength="30" oninvalid="this.setCustomValidity('Please, submit required data')"
-            oninput="this.setCustomValidity('')">
-            <br>
-            <label for="name" id="name">Name</label>
-            <input type="text" id="name" name="name" required="required" maxlength="30">
-            <br>
-            <label for="price" id="price">Price($)</label>
-            <input type="number" id="price" step="0.01" name="price" required="required" maxlength="30">
-            <br>
-            <label for="productType">Type Switcher</label>
-            <select name="productType" id="productType" required="required" onchange="prodTypeSelection(this.value)">
-                <option value="">Type Switcher</option>
-                <option value="dvd" class="dvd">DVD</option>
-                <option value="book" class="book">Book</option>
-                <option value="furniture" class="furniture">Furniture</option>
-            </select>
-            <br>
+                <div class="mb-3">
+                <label for="sku" id="sku">SKU</label>
+                <input type="text" id="sku" name="sku" maxlength="30" oninvalid="this.setCustomValidity('Please, submit required data')"
+                oninput="this.setCustomValidity('')">
+                </div>
+
+                <div class="mb-3">
+                <label for="name" id="name">Name</label>
+                <input type="text" id="name" name="name" maxlength="30">
+                </div>
+
+                <div class="mb-3">
+                <label for="price" id="price">Price($)</label>
+                <input type="number" id="price" step="0.01" name="price" maxlength="30">
+                </div>
+                
+                <div class="input-group mb-3">
+                <label for="productType">Type Switcher</label>
+                <select name="productType" class="form-select" id="productType" onchange="prodTypeSelection(this.value)">
+                        <option value="">Type Switcher</option>
+                        <option value="dvd" class="dvd">DVD</option>
+                        <option value="book" class="book">Book</option>
+                        <option value="furniture" class="furniture">Furniture</option>
+                    </select>
+                </div>
              
-                <div class="fieldbox" id="dvd_attributes">
+                <div class=" mb-3 fieldbox" id="dvd_attributes">
                 <label>Size (MB)</label>
                 <input type="number" name="dvd_attributes" id="size" step="0.01" value="" maxlength="30">
                 <div id="description">Please, provide size!</div>
                 </div>
 
-                <div class="fieldbox" id="book_attributes">
+                <div class=" mb-3 fieldbox" id="book_attributes">
                 <label>Weight (KG)</label>
                 <input type="number" id="weight" name="book_attributes" step="0.01" value="" maxlength="30">
                 <div id="description">Please, provide weight!</div>
                 </div>
 
-                <div class="fieldbox" id="furniture_attributes">
+                <div class="mb-3 fieldbox" id="furniture_attributes">
                 <label for="height">Height (CM)</label>
                 <input type="number" name="furniture_attributes[0]" id="height" step="0.01" maxlength="30">
                 <br>
@@ -113,11 +133,15 @@
            
             
         </form>
+</div>
 
     </body>
     <script src="javascriptfunc.js"></script>
-    <br>
-    <hr>
-    <footer>Scandiweb Test assigment</footer>
+    <!-- Footer -->
+     <hr>   
+    <div id="footer">
+        <footer>Scandiweb Test assigment</footer>
+        <p>© Copyright Laura Griga 2022</p>
+    </div>
 </html>
 
--- productlist.php
+++ productlist.php
@@ -3,70 +3,97 @@
         <title>Product list</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Google fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+
+        <!--Stylesheets-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     </head>
+
     <body>
 
-<!-- Mass delete section-->
-<?php 
-require_once 'connectdb.php';
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        try {
-            $ids = array_keys($_POST['deleteId']);
-            $selected = implode(", ", $ids);
-            $stmt = $conn->prepare("DELETE FROM productlist WHERE id in ($selected)");
-                
-            $stmt->execute();
+        <!-- Mass delete section-->
+        <?php 
+        require_once 'connectdb.php';
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                try {
+                    $ids = array_keys($_POST['deleteId']);
+                    $selected = implode(", ", $ids);
+                    $stmt = $conn->prepare("DELETE FROM productlist WHERE id in ($selected)");
+                        
+                    $stmt->execute();
 
-            } catch (PDOException $e) {
-                echo "<p>Error: " . $e->getMessage() . "</p>";
-            }
+                    } catch (PDOException $e) {
+                        echo "<p>Error: " . $e->getMessage() . "</p>";
+                    }
+                }
+        ?>
+
+        <!-- Productlist -->
+        <?php 
+        require_once 'connectdb.php';
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
         }
-?>
-<!-- Productlist -->
-<?php 
-require_once 'connectdb.php';
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-}
+        try {
+            $stmt = $conn->prepare("SELECT * FROM productlist");
+            $stmt->execute();
+            $productlist = $stmt->fetchAll(); ?>
+            <form id="product_form" action="productlist.php" method="post" name="massdelete" >
 
-try {
-    $stmt = $conn->prepare("SELECT * FROM productlist");
-    $stmt->execute();
-    $productlist = $stmt->fetchAll(); ?>
-    <form id="product_form" action="productlist.php" method="post" name="massdelete" >
+            <div id="title">
+                <nav class="navbar">
+                    <div class="container-fluid">
+                    <a class="navbar-brand">Product list</a>
+                    <span class="navbar-item">
+                    <button class="btn btn-light btn-style" type="button" onclick="window.location.href='add-product.php'">ADD</button>
+                    <button class="btn btn-light btn-style" type="submit" id="delete-product-btn"> MASS DELETE</button>
+                    </div>
+                </nav>
+            </div>
 
-    <!--Need to format with css that text and buttons is in one line-->
-    <nav>
-        <h3>Product list</h3>
-        <div class = "buttonposition">
-        <button type="button" onclick="window.location.href='add-product.php'">ADD</button>
-        <button type="submit" id="delete-product-btn"> MASS DELETE</button>
-        </div>
-</nav>
-<hr>
-        <div class="flex-container">
-            <?php foreach ($productlist as $pl) : ?>
-                <div class="flex-div">
-                    <input type="checkbox" id="delete-checkbox" class="delete-checkbox" name="deleteId[<?=$pl['id']?>]"><br>
-                    <?=$pl['sku']?><br>
-                    <?=$pl['name']?><br>
-                    <?=$pl['price']?> $<br>
-                    <!--Need to add information before and after attribute depending of ProductType -->
-                    <?=$pl['attributes']?>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </form>
-    <?php
-} catch (PDOException $e) {
-    echo "<p>Error: " . $e->getMessage() . "</p>";
-}
+        <hr>
 
-$conn = null;
-?>
+            <div id="body">
+                    <div class="row">
+                        <?php foreach ($productlist as $pl) : ?>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                    <input type="checkbox" id="delete-checkbox" class="delete-checkbox" name="deleteId[<?=$pl['id']?>]"><br>
+                                    <?=$pl['sku']?><br>
+                                    <?=$pl['name']?><br>
+                                    <?=$pl['price']?> $<br>
+                                    <!--Need to add information before and after attribute depending of ProductType -->
+                                    <?=$pl['attributes']?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </form>
+                    <?php
+                } catch (PDOException $e) {
+                    echo "<p>Error: " . $e->getMessage() . "</p>";
+                }
+
+                $conn = null;
+                ?>
+            </div>
     </body>
-    <br>
     <hr>
-    <footer>Scandiweb Test assigment</footer>
+    
+    <script src="javascriptfunc.js"></script>
+   
+    <!-- Footer -->
+        
+        <div id="footer">
+        <footer>Scandiweb Test assigment</footer>
+        <p>© Copyright Laura Griga 2022</p>
+    </div>
 </html>
